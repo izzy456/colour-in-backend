dc62c07884c1422bc23e4c148a40faa7003e5425
Isabella Hogg <29220416+Isabella97H@users.noreply.github.com>
2024-01-06 19:44:31 +1000

    rename aws dir to ecs

--- ecs/deploy.sh
+++ ecs/deploy.sh
@@ -5,4 +5,3 @@ aws ecs register-task-definition --cli-input-json file://taskdef_$TAG.json
 echo "Update ECS service"
 aws ecs update-service --cluster $CLUSTER --service $SERVICE \
     --task-definition colour-in-backend:$(aws ecs describe-task-definition --task-definition colour-in-backend | jq ".taskDefinition.revision") --desired-count 2
-
