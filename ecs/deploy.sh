a5820808b62db4580efbf5885faac42d7c2c45bf
Isabella Hogg <29220416+Isabella97H@users.noreply.github.com>
2024-01-07 23:51:53 +1000

    update deploy script

--- ecs/deploy.sh
+++ ecs/deploy.sh
@@ -3,5 +3,11 @@ echo "Register ECS task definition"
 aws ecs register-task-definition --cli-input-json file://taskdef_$TAG.json
 
 echo "Update ECS service"
-aws ecs update-service --cluster $CLUSTER --service $SERVICE \
-    --task-definition colour-in-backend:$(aws ecs describe-task-definition --task-definition colour-in-backend | jq ".taskDefinition.revision") --desired-count 2
+if [[ "${DESIRED_COUNT}" == "" ]]
+then
+    aws ecs update-service --cluster $CLUSTER --service $SERVICE \
+        --task-definition colour-in-backend:$(aws ecs describe-task-definition --task-definition colour-in-backend | jq ".taskDefinition.revision")
+else
+    aws ecs update-service --cluster $CLUSTER --service $SERVICE \
+        --task-definition colour-in-backend:$(aws ecs describe-task-definition --task-definition colour-in-backend | jq ".taskDefinition.revision") --desired-count $DESIRED_COUNT
+fi
